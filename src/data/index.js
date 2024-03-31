f50490f8dbfb267f00a9b2c11ed7efe5f90ee6c2
kiran385 <145046362+kiran385@users.noreply.github.com>
2024-03-31 09:14:33 +0530

    Add files via upload

---
+++ src/data/index.js
@@ -0,0 +1,39 @@
+const employeesData = [
+    {
+      id: 1,
+      firstName: 'Akash',
+      lastName: 'Patel',
+      email: 'akashpatel@gmail.com',
+      salary: '90000',
+    },
+    {
+      id: 2,
+      firstName: 'Anil',
+      lastName: 'Pandya',
+      email: 'anilpandya@gmail.com',
+      salary: '80000',
+    },
+    {
+      id: 3,
+      firstName: 'Rahul',
+      lastName: 'Chauhan',
+      email: 'rahulchauhan@gmail.com',
+      salary: '70000',
+    },
+    {
+      id: 4,
+      firstName: 'Keval',
+      lastName: 'Rathod',
+      email: 'kevalrathod@gmail.com',
+      salary: '50000',
+    },
+    {
+      id: 5,
+      firstName: 'Ankit',
+      lastName: 'Sharma',
+      email: 'ankitsharma@gmail.com',
+      salary: '65000',
+    }
+  ];
+  
+  export { employeesData };
